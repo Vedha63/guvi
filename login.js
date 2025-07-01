30870fed1e8c4ccb9cc62504d140da2a4696749e
vedha ponnam <245122749040@mvsrec.edu.in>
2025-07-02 00:48:06 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,1 +1,22 @@
-...
+$(document).ready(function () {
+  $('#loginForm').submit(function (e) {
+    e.preventDefault();
+    $.ajax({
+      url: 'php/login.php',
+      method: 'POST',
+      dataType: 'json',
+      data: {
+        email: $('#email').val(),
+        password: $('#password').val()
+      },
+      success: function (res) {
+        if (res.status === 'success') {
+          localStorage.setItem('userSession', JSON.stringify({ email: res.email }));
+          window.location.href = 'profile.html';
+        } else {
+          alert(res.message);
+        }
+      }
+    });
+  });
+});
