de342b9215d52911ce16fd53694f95b42e6e5362
vedha ponnam <245122749040@mvsrec.edu.in>
2025-07-02 00:44:12 +0530

    Update register.js

--- register.js
+++ register.js
@@ -1,1 +1,20 @@
-...
+$(document).ready(function () {
+  $('#registerForm').submit(function (e) {
+    e.preventDefault();
+    $.ajax({
+      url: 'php/register.php',
+      method: 'POST',
+      data: {
+        name: $('#name').val(),
+        email: $('#email').val(),
+        password: $('#password').val()
+      },
+      success: function (response) {
+        alert(response);
+        if (response === 'Registered successfully') {
+          window.location.href = 'index.html';
+        }
+      }
+    });
+  });
+});
